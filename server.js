56b9a0528067a95c057260bffd1fae41a81bf929
Ayush Raj <ping2r3j@gmail.com>
2020-03-28 21:53:28 +0530

    some changes

--- server.js
+++ server.js
@@ -4,22 +4,26 @@ var app = express();
 var covid = require('novelcovid');
 var path = require('path');
 app.use(cors());
-var port = process.env.PORT||3000;
+var port = process.env.PORT||8080;
 
 app.use(express.static(__dirname + '/dist'));
 
 var errHandler = function(err) {
     console.log(err);
 }
+
+app.get('/*',(req, res)=> {
+    res.sendFile(path.join(__dirname+'/dist/index.html'));
+});
+
 app.get('/api/deathsAll',(req, res, next)=> {
     var dataPromise = covid.getAll();
     dataPromise.then(function(result) {
                     res.send(result);
                 }, errHandler)
 });
-app.get('/',(req, res)=> {
-    res.sendFile(path.join(__dirname+'/dist/index.html'));
-});
+
+
 app.get('/api/deathsCountry',(req, res, next)=> {
     var dataPromise = covid.getCountry();
     dataPromise.then(function(result) {
